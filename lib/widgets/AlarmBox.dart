ca62809b1c116f49b121787f94a978a020f240c4
ddoong10 <ddoong10@naver.com>
2025-01-13 09:31:33 +0900

    alarm_save

--- lib/widgets/AlarmBox.dart
+++ lib/widgets/AlarmBox.dart
@@ -1,37 +1,50 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/cupertino.dart';
-// pubspec.yaml에 flutter_slidable 추가 후 사용
 import 'package:flutter_slidable/flutter_slidable.dart';
+import 'package:projects/utils/DataStorage.dart';
 
 class AlarmBox extends StatefulWidget {
-  const AlarmBox({super.key});
+  final String title;
+  final String time;
+  final String location;
+  final bool isOn;
+  final int index;
+
+  const AlarmBox({
+    Key? key,
+    required this.title,
+    required this.time,
+    required this.location,
+    required this.isOn,
+    required this.index,
+  }) : super(key: key);
 
   @override
   State<AlarmBox> createState() => _AlarmBoxState();
 }
 
 class _AlarmBoxState extends State<AlarmBox> {
-  bool isChecked = false;
+  late bool isChecked;
+
+  @override
+  void initState() {
+    super.initState();
+    isChecked = widget.isOn;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      margin: const EdgeInsets.fromLTRB(16, 16, 16, 0), // 위로 16px 마진
+      margin: const EdgeInsets.fromLTRB(16, 16, 16, 0),
       child: Slidable(
-        // 슬라이드 구분 키
-        key: const ValueKey('AlarmBox'),
-        // 가로 방향 슬라이드
-        direction: Axis.horizontal,
-        // 오른쪽(END)에 액션 패널을 배치해서 '왼쪽으로' 스와이프
+        key: ValueKey(widget.index),
         endActionPane: ActionPane(
-          // 슬라이드 폭을 전체의 (50/343)만큼만 보여주도록 설정
           extentRatio: 78 / 343,
           motion: const DrawerMotion(),
           children: [
-            // 삭제 액션
             SlidableAction(
               onPressed: (context) {
-                debugPrint('삭제 버튼 클릭');
+                DataStorage.deleteAlarm(widget.index);
               },
               backgroundColor: const Color(0xFF5EB6FF),
               foregroundColor: Colors.white,
@@ -45,15 +58,11 @@ class _AlarmBoxState extends State<AlarmBox> {
         ),
         child: InkWell(
           borderRadius: BorderRadius.circular(10),
-          onTap: () {
-            debugPrint('알림 박스 클릭');
-          },
           child: SizedBox(
             width: 343,
             height: 83,
             child: Stack(
               children: [
-                // 알림 상자 배경
                 Container(
                   width: 343,
                   height: 83,
@@ -64,69 +73,31 @@ class _AlarmBoxState extends State<AlarmBox> {
                     ),
                   ),
                 ),
-                // 시각 (08:00 / PM)
-                const Positioned(
+                Positioned(
                   left: 14,
                   top: 9,
                   child: Text(
-                    '08:00',
-                    style: TextStyle(
+                    widget.time,
+                    style: const TextStyle(
                       color: Color(0xFF5EB6FF),
                       fontSize: 32,
                       fontWeight: FontWeight.w600,
                     ),
                   ),
                 ),
-                const Positioned(
-                  left: 105,
-                  top: 24,
-                  child: Text(
-                    'PM',
-                    style: TextStyle(
-                      color: Color(0xFF5EB6FF),
-                      fontSize: 16,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                ),
-                // 텍스트: 반지하 돈부리 · 저녁 약속
                 Positioned(
                   left: 13.5,
                   top: 47,
-                  child: Row(
-                    children: [
-                      const Text(
-                        '반지하 돈부리',
-                        style: TextStyle(
-                          color: Color(0xFFC2C2C2),
-                          fontSize: 14,
-                          fontWeight: FontWeight.w400,
-                          letterSpacing: -0.80,
-                        ),
-                      ),
-                      const SizedBox(width: 4),
-                      Container(
-                        width: 4,
-                        height: 4,
-                        decoration: const ShapeDecoration(
-                          color: Colors.black26,
-                          shape: OvalBorder(),
-                        ),
-                      ),
-                      const SizedBox(width: 4),
-                      const Text(
-                        '저녁 약속',
-                        style: TextStyle(
-                          color: Color(0xFFC2C2C2),
-                          fontSize: 14,
-                          fontWeight: FontWeight.w400,
-                          letterSpacing: -0.80,
-                        ),
-                      ),
-                    ],
+                  child: Text(
+                    '${widget.location} · ${widget.title}',
+                    style: const TextStyle(
+                      color: Color(0xFFC2C2C2),
+                      fontSize: 14,
+                      fontWeight: FontWeight.w400,
+                      letterSpacing: -0.80,
+                    ),
                   ),
                 ),
-                // 스위치
                 Positioned(
                   left: 283,
                   top: 25,
@@ -135,7 +106,10 @@ class _AlarmBoxState extends State<AlarmBox> {
                     activeColor: const Color(0xFF5FB7FF),
                     trackColor: const Color(0xFF757575),
                     onChanged: (bool value) {
-                      setState(() => isChecked = value);
+                      setState(() {
+                        isChecked = value;
+                      });
+                      DataStorage.updateAlarmStatus(widget.index, value);
                     },
                   ),
                 ),
@@ -143,6 +117,58 @@ class _AlarmBoxState extends State<AlarmBox> {
             ),
           ),
         ),
+      ),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  List<Map<String, dynamic>> alarms = [];
+
+  @override
+  void initState() {
+    super.initState();
+    loadAlarms();
+  }
+
+  void loadAlarms() async {
+    final loadedAlarms = await DataStorage.loadAlarms();
+    setState(() {
+      alarms = loadedAlarms;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('알람 목록'),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.refresh),
+            onPressed: () => loadAlarms(),
+          ),
+        ],
+      ),
+      body: ListView.builder(
+        itemCount: alarms.length,
+        itemBuilder: (context, index) {
+          final alarm = alarms[index];
+          return AlarmBox(
+            index: index,
+            title: alarm['title'],
+            time: alarm['time'],
+            location: alarm['location'],
+            isOn: alarm['isOn'],
+          );
+        },
       ),
     );
   }
